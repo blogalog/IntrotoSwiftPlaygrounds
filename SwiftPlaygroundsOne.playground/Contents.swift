0dee5c7b22d226ad27e48bbe8c6ad4ba8e6375f7
Brandon <blogalog@hawaii.edu>
2019-01-18 12:57:06 -1000

    Created Variables and Strings

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -1,3 +1,7 @@
 import UIKit
 
 var str = "Hello, playground"
+var myFirstVariable = "I've created my first variable"
+var mySecondVariable = "and look I added 2 strings together"
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
